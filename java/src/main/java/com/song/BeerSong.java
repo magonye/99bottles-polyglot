2572cfdc04d27dab7efd71c6b429861ff80d2184
miguel.gonzalez <magonye@gmail.com>
2018-03-02 13:03:26 +0100

    singFirstTwoVerses

--- java/src/main/java/com/song/BeerSong.java
+++ java/src/main/java/com/song/BeerSong.java
@@ -15,7 +15,13 @@ public class BeerSong {
                 "Take one down and pass it around, %s %s of beer on the wall.\n\n";
         String bottleWord = bottlesNumber == 2 ? "bottle" : "bottles";
 
-        return String.format(frase, bottlesNumber, bottlesNumber, bottlesNumber-1, bottleWord);
+        String result = String.format(frase, bottlesNumber, bottlesNumber, bottlesNumber-1, bottleWord);
+
+        if (versesNumber == 2) {
+            result += "98 bottles of beer on the wall, 98 bottles of beer.\n" + "Take one down and pass it around, 97 bottles of beer on the wall.\n\n";
+        }
+
+        return result;
     }
 
     public String singSong() {
