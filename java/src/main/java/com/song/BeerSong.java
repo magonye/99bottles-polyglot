4c29698880e2000aab06e4cbc0c102784284ba82
miguel.gonzalez <magonye@gmail.com>
2018-03-02 12:49:54 +0100

    verseWithOneBottle

--- java/src/main/java/com/song/BeerSong.java
+++ java/src/main/java/com/song/BeerSong.java
@@ -3,6 +3,10 @@ package com.song;
 public class BeerSong {
 
     public String sing(int bottlesNumber, int versesNumber) {
+        if (bottlesNumber ==1){
+            return "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                    "Take it down and pass it around, no more bottles of beer on the wall.\n\n";
+        }
         String frase = "%s bottles of beer on the wall, %s bottles of beer.\n" +
                 "Take one down and pass it around, %s %s of beer on the wall.\n\n";
         String bottleWord = bottlesNumber == 2 ? "bottle" : "bottles";
